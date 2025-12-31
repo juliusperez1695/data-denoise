7b84eab132cd2c642b4e858f7253ffed3d0d64be
Julius Perez <juliusperez1695@gmail.com>
2025-12-30 17:08:39 -0800

    Build and Test Workflow with artifacts

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+setup(
+    # This setup.py is primarily for backward compatibility.
+    # Configuration is read from pyproject.toml.
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+)
 
 if __name__ == "__main__":
     setup()
