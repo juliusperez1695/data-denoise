ea6295f79222acb134f623a399d7efae2a988145
Julius Perez <juliusperez1695@gmail.com>
2026-01-20 19:53:54 -0800

    Added parabola fit unit test

--- tests/test_outlier_identification.py
+++ tests/test_outlier_identification.py
@@ -16,7 +16,7 @@ def test_parabola_outliers():
     test_df = pd.read_csv(datapath, header=None, float_precision='round_trip')
     dataprocessor = DataProcessor()
     dataprocessor.set_orig_data(test_df)
-    outlier_idxs = dataprocessor.identify_outliers(fit_mode = 1)
+    outlier_idxs = dataprocessor.identify_outliers_iterative(fit_mode = 1)
     num_outliers = len(outlier_idxs)
 
     assert len(outlier_idxs) == 2, f"FAILED Parabola Outliers Test 1.  Number of outliers: {num_outliers}, Expected: {2}."
@@ -27,7 +27,7 @@ def test_parabola_outliers():
     test_df = pd.read_csv(datapath, header=None, float_precision='round_trip')
     dataprocessor = DataProcessor()
     dataprocessor.set_orig_data(test_df)
-    outlier_idxs = dataprocessor.identify_outliers(fit_mode = 1)
+    outlier_idxs = dataprocessor.identify_outliers_iterative(fit_mode = 1)
     num_outliers = len(outlier_idxs)
 
     assert len(outlier_idxs) == 3, f"FAILED Parabola Outliers Test 2.  Number of outliers: {num_outliers}, Expected: {3}."
