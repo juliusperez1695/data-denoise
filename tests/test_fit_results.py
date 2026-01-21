d4fb56829d28b6a7b28a375d87878bef3abb3966
Julius Perez <juliusperez1695@gmail.com>
2026-01-20 21:12:28 -0800

    Added parabola fit unit test

--- tests/test_fit_results.py
+++ tests/test_fit_results.py
@@ -24,4 +24,4 @@ def test_parabola_fit():
     fit_type = dataprocessor._get_fit_type(fit_mode = 1)
     _, fit_params = dataprocessor.get_fit_values(fit_type, x_vals, y_vals)
 
-    assert np.array_equal(test_coeffs, fit_params), f"FAILED Parabola Fit. Coefficients (Fit) = {test_coeffs}, Coefficients (Expected) = {fit_params}."
+    assert np.allclose(test_coeffs, fit_params), f"FAILED Parabola Fit. Coefficients (Fit) = {test_coeffs}, Coefficients (Expected) = {fit_params}."
